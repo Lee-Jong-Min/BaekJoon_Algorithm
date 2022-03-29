3cf7fb33e6a21c72c188dd17f8624854cef11817
JongMin Lee <2314ljm@gmail.com>
2022-03-29 11:25:09 +0900

    absolute_value_heap

---
+++ absolute_value_heap.py
@@ -0,0 +1,17 @@
+from re import I
+import sys
+import heapq
+
+test_case = int(sys.stdin.readline())
+heap = []
+
+for i in range(test_case):
+    num = int(sys.stdin.readline())
+    if num != 0:
+        heapq.heappush(heap,(abs(num),num))
+    else:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+        
