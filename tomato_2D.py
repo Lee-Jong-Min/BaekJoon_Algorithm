2ff2646f38dceb1df1eea12e52e993cc5e13c932
JongMin Lee <2314ljm@gmail.com>
2022-05-07 23:59:54 +0900

    tomato_2D

--- tomato_2D.py
+++ tomato_2D.py
@@ -1,57 +1,51 @@
-def bfs(x,y):
-    queue = deque()
-    queue.append((x,y))
+def bfs():
+    
     cnt = 0
     
     while queue:
-        x,y = queue.popleft()
-        l = 1
-        
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        cnt += 1
+        for _ in range(len(queue)):
+            x,y = queue.popleft()
             
-            if nx < 0 or nx >= N or ny < 0 or ny >= M:
-                continue
-            elif graph[nx][ny] == 0:
-                queue.append((nx,ny))
-                if graph[x][y] == 1:
-                    graph[nx][ny] = 1
-                    if l == 1:
-                        cnt += 1
-                elif graph[x][y] == -1:
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+            
+                if nx < 0 or nx >= N or ny < 0 or ny >= M:
                     continue
-            elif graph[nx][ny] == 1:
-                continue
-            
-    return cnt
+                if graph[nx][ny] == 0:
+                    queue.append((nx,ny))
+                    graph[nx][ny] = 1
+                if graph[x][y] == -1:
+                    continue
+    return cnt-1
 
 import sys
 from collections import deque
 M,N = map(int,sys.stdin.readline().split())
 
-graph = []
-
-for i in range(N):
-    graph.append(list(map(int,sys.stdin.readline().split())))
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
     
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
+queue = deque()
+    
 for i in range(N):
     for j in range(M):
         if graph[i][j] == 1:
-            start_x,start_y = i,j
+            queue.append((i,j))
 
-a = bfs(start_x,start_y)
-b = 1
+a = bfs()
+flag = 1
+
 if a == 0:
     print(0)
 else:
-    for i in range(N):
-        if graph[i].count(0) != 0:
+    for i in graph:
+        if 0 in i:
             print(-1)
-            b = 0
+            flag = 0
             break
-    if b == 1:
+    if flag == 1:
         print(a)
